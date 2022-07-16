3258f1a3a83c22d5632fd22d06ed363d21846305
Kwabena Opoku Frempong-Kore <kobekore@gmail.com>
2022-07-16 11:49:31 +0000

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,10 +9,10 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, j;
+	size_t i, j;
 	int tmp;
 
-	for (; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 
 		for (j = 0; j < size - 1 - i; j++)
